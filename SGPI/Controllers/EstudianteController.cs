b6e45e22a6943ed28eece302d44837bfab0963f1
Orley668 <114945675+Orley668@users.noreply.github.com>
2022-11-14 19:18:22 -0500

    actualizacion 1

---
+++ SGPI/Controllers/EstudianteController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPI.Models;
+using System.Linq;
+
+namespace SGPI.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        SGPIDBContext context = new SGPIDBContext();
+     
+        public IActionResult ActualizarEstudiante()
+        {
+          
+                ViewBag.genero = context.Generos.ToList();
+                ViewBag.documento = context.Documentos.ToList();
+                ViewBag.rol = context.Rols.ToList();
+                ViewBag.programa = context.Programas.ToList();
+                return View();
+            
+           
+        }
+
+        [HttpPost]
+        public IActionResult ActualizarEstudiante(Usuario user)
+        {
+            context.Update(user);
+            context.SaveChanges();
+            ViewBag.genero = context.Generos.ToList();
+            ViewBag.documento = context.Documentos.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            return View();
+
+
+        }
+
+        public IActionResult PagosEstudiante()
+        {
+        
+          
+            return View();
+        }
+       
+    }
+}
+
+    
+
---
+++ SGPI/Models/Pago.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SGPI.Models
+{
+    public partial class Pago
+    {
+        public Pago()
+        {
+            Estudiantes = new HashSet<Estudiante>();
+        }
+
+        public int IdPago { get; set; }
+        public int ValorPago { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Estado { get; set; }
+
+        public virtual ICollection<Estudiante> Estudiantes { get; set; }
+
+
+        create proc categoria_existe
+        @valor varchar(100),
+@existe bit output
+as
+       if exists(select nombre from categoria where categoria = ltrim(rtrim(@valor)))
+             begin
+              set @existe=1
+             end
+       else
+             begin
+              set @existe=0
+             end
+    }
+}
